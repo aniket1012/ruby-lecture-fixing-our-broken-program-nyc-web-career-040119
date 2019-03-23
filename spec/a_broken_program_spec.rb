b9d049f6b2fc7eeaa5385a6ca3b9d5f3edfd68ba
Aniket Patel <aniketupatel1@gmail.com>
2019-03-23 18:00:25 +0000

    Automatically backed up by Learn

--- spec/a_broken_program_spec.rb
+++ spec/a_broken_program_spec.rb
@@ -3,7 +3,7 @@ describe 'A broken program' do
   it 'not to raises an error when loaded' do
     expect{
       load 'lib/a_broken_program.rb'
-    }.not to raise_error
+    }.to_not raise_error
   end
 
 end
